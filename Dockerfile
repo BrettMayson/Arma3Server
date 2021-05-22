b72b62c54a2fffa1352eb123dc4fbc910590c434
Markus Appel <markusappel@hotmail.de>
2021-05-22 23:11:20 +0200

    WIP

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,17 @@
 FROM debian:buster-slim
 
-LABEL maintainer="Brett - github.com/synixebrett"
+LABEL maintainer="Markus"
 
 RUN apt-get update \
     && \
     apt-get install -y --no-install-recommends --no-install-suggests \
         python3 \
+        python3-setuptools \
         lib32stdc++6 \
         lib32gcc1 \
         wget \
         ca-certificates \
+        python3-pip \
     && \
     apt-get remove --purge -y \
     && \
@@ -23,6 +25,9 @@ RUN apt-get update \
         && cd /steamcmd \
         && wget -qO- 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz' | tar zxf -
 
+
+RUN pip3 install boto3
+
 ENV ARMA_BINARY=./arma3server
 ENV ARMA_CONFIG=main.cfg
 ENV ARMA_PROFILE=main
@@ -32,6 +37,8 @@ ENV HEADLESS_CLIENTS=0
 ENV PORT=2302
 ENV STEAM_BRANCH=public
 ENV STEAM_BRANCH_PASSWORD=
+
+ENV DATA_BUCKET=
 
 EXPOSE 2302/udp
 EXPOSE 2303/udp
--- launch.py
+++ launch.py
@@ -2,27 +2,49 @@ import subprocess
 import os
 import shutil
 import re
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import socket
+import boto3
+import multiprocessing
+import zipfile
+import time
 
 CONFIG_FILE = os.environ["ARMA_CONFIG"]
 KEYS = "/arma3/keys"
+DATA_BUCKET = os.environ["DATA_BUCKET"]
 
-if not os.path.exists(KEYS) or not os.path.isdir(KEYS):
-    if os.path.exists(KEYS):
-        os.remove(KEYS)
-    os.makedirs(KEYS)
+def reset_keys_folder():
+    if not os.path.exists(KEYS) or not os.path.isdir(KEYS):
+        if os.path.exists(KEYS):
+            os.remove(KEYS)
+        os.makedirs(KEYS)
 
-steamcmd = ["/steamcmd/steamcmd.sh"]
-steamcmd.extend(["+login", os.environ["STEAM_USER"], os.environ["STEAM_PASSWORD"]])
-steamcmd.extend(["+force_install_dir", "/arma3"])
-steamcmd.extend(["+app_update", "233780"])
-if "STEAM_BRANCH" in os.environ and len(os.environ["STEAM_BRANCH"]) > 0:
-    steamcmd.extend(["-beta", os.environ["STEAM_BRANCH"]])
-if "STEAM_BRANCH_PASSWORD" in os.environ and len(os.environ["STEAM_BRANCH_PASSWORD"]) > 0:
-    steamcmd.extend(["-betapassword", os.environ["STEAM_BRANCH_PASSWORD"]])
-steamcmd.extend(["validate", "+quit"])
-subprocess.call(steamcmd)
+def run_healthcheck_server():
+    host = ''        # Symbolic name meaning all available interfaces
+    port = 12345     # Arbitrary non-privileged port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((host, port))
+    while True:
+        s.listen(1)
+        conn, addr = s.accept()
+        print('Connected by', addr)
+        conn.sendall("healthy".encode())
+        conn.close()
+        print('Closed connection', addr)
 
-def mods(d):
+def init_steamcmd():
+    steamcmd = ["/steamcmd/steamcmd.sh"]
+    steamcmd.extend(["+login", os.environ["STEAM_USER"], os.environ["STEAM_PASSWORD"]])
+    steamcmd.extend(["+force_install_dir", "/arma3"])
+    steamcmd.extend(["+app_update", "233780"])
+    if "STEAM_BRANCH" in os.environ and len(os.environ["STEAM_BRANCH"]) > 0:
+        steamcmd.extend(["-beta", os.environ["STEAM_BRANCH"]])
+    if "STEAM_BRANCH_PASSWORD" in os.environ and len(os.environ["STEAM_BRANCH_PASSWORD"]) > 0:
+        steamcmd.extend(["-betapassword", os.environ["STEAM_BRANCH_PASSWORD"]])
+    steamcmd.extend(["validate", "+quit"])
+    subprocess.call(steamcmd)
+
+def build_mods_string(d):
     launch = "\""
     mods = [os.path.join(d,o) for o in os.listdir(d) if os.path.isdir(os.path.join(d,o))]
     for m in mods:
@@ -36,52 +58,60 @@ def mods(d):
             print("Missing keys:", keysdir)
     return launch+"\""
 
+def print_subprocess(proc):
+    while True:
+        time.sleep(1)
+        while proc.stdout.readable():
+            line = proc.stdout.readline()
+            if not line:
+                break
+            print(proc.pid + ': ' + line.strip())
+
+def await_death(proc):
+    while True:
+        time.sleep(1)
+        try:
+            # Test if pid accepts sig 0 (nothing will happen)
+            os.kill(proc.pid, 0)
+        except OSError:
+            print("Process isn't alive", proc.pid)
+            break
+
+def download_data():
+    s3 = boto3.client('s3')
+    s3.download_file(DATA_BUCKET, "mods.zip", "mods.zip")
+    with zipfile.ZipFile("mods.zip", 'r') as zip_ref:
+        zip_ref.extractall("/arma3/mods")
+
+############################################################################################################################
+
+healthcheck_process = multiprocessing.Process(target=run_healthcheck_server)
+healthcheck_process.start()
+
+reset_keys_folder()
+init_steamcmd()
+
 launch = "{} -limitFPS={} -world={}".format(os.environ["ARMA_BINARY"], os.environ["ARMA_LIMITFPS"], os.environ["ARMA_WORLD"])
 
 if os.path.exists("mods"):
-    launch += " -mod={}".format(mods("mods"))
+    launch += " -mod={}".format(build_mods_string("mods"))
 
-clients = int(os.environ["HEADLESS_CLIENTS"])
-
-print("Headless Clients:", clients)
-
-if clients != 0:
-    with open("/arma3/configs/{}".format(CONFIG_FILE)) as config:
-        data = config.read()
-        regex = r"(.+?)(?:\s+)?=(?:\s+)?(.+?)(?:$|\/|;)"
-
-        config_values = {}
-
-        matches = re.finditer(regex, data, re.MULTILINE)
-        for matchNum, match in enumerate(matches, start=1):
-            config_values[match.group(1).lower()] = match.group(2)
-
-        if not "headlessclients[]" in config_values:
-            data += "\nheadlessclients[] = {\"127.0.0.1\"};\n"
-        if not "localclient[]" in config_values:
-            data += "\nlocalclient[] = {\"127.0.0.1\"};\n"
-
-        with open("/tmp/arma3.cfg", "w") as tmp_config:
-            tmp_config.write(data)
-        launch += " -config=\"/tmp/arma3.cfg\""
-
-    
-    client_launch = launch
-    client_launch += " -client -connect=127.0.0.1"
-    if "password" in config_values:
-        client_launch += " -password={}".format(config_values["password"])
-
-    for i in range(0, clients):
-        print("LAUNCHING ARMA CLIENT {} WITH".format(i), client_launch)
-        subprocess.Popen(client_launch, shell=True)
-
-else:
-    launch += " -config=\"/arma3/configs/{}\"".format(CONFIG_FILE)
-
+launch += " -config=\"/arma3/configs/{}\"".format(CONFIG_FILE)
 launch += " -port={} -name=\"{}\" -profiles=\"/arma3/configs/profiles\"".format(os.environ["PORT"], os.environ["ARMA_PROFILE"])
 
 if os.path.exists("servermods"):
-    launch += " -serverMod={}".format(mods("servermods"))
+    launch += " -serverMod={}".format(build_mods_string("servermods"))
 
 print("LAUNCHING ARMA SERVER WITH", launch, flush=True)
-os.system(launch)
+proc = subprocess.Popen(launch, shell=True)
+
+def print_proc():
+    print_subprocess(proc)
+
+output_process = multiprocessing.Process(target=print_proc)
+output_process.start()
+
+await_death(proc)
+
+healthcheck_process.terminate()
+output_process.terminate()
